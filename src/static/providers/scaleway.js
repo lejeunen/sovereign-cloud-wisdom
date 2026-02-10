79b004b7e5276bd3d2651b0eaec1d3078aadbce8
Nicolas Lejeune <lejeune.n@gmail.com>
2026-02-10 18:35:48 +0100

    feat: add GitHub repository links in footer

--- src/static/providers/scaleway.js
+++ src/static/providers/scaleway.js
@@ -1,6 +1,9 @@
 const PROVIDER = {
     name: "Scaleway",
     repo: "lejeunen/scaleway-starter-kit",
+    repoUrl: function() {
+        return "https://github.com/" + this.repo;
+    },
     complianceUrl: function(lang) {
         const suffix = lang === "fr" ? ".fr" : "";
         return "https://raw.githubusercontent.com/" + this.repo + "/main/COMPLIANCE" + suffix + ".md";
